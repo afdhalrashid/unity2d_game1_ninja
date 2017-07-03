5be2d7ff03d360a154f5ddd224870c5d59486080
afdhal-PC\afdhal <awakenblueheart@gmail.com>
2017-07-03 16:02:00 +0800

    End of this Youtube video : https://www.youtube.com/watch?v=HpJMhNmpIxY I learn to 1.Move character 2.SerializeField  (add this in c sharp script to create a new attribute/properties in Unity object which added on the Unity properties (Inspector) and can be change there) 3.Update function change to FixedUpdate (to equalize the frame rate for each computer either powerful or slower PC)

---
+++ first_game/FirstGame/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D myrigidBody;
+    [SerializeField]
+    private float character_speed = 3;
+
+	// Use this for initialization
+	void Start () {
+        myrigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float Horizontal = Input.GetAxis("Horizontal");
+
+        //if (Horizontal < 0 ) {
+        //    Horizontal -= 3;
+        //}
+        
+        //if (Horizontal > 0 ) {
+        //    Horizontal += 3;
+        //}
+        Debug.Log(Horizontal);
+
+        HandleMovement(Horizontal);
+	}
+
+    private void HandleMovement(float h)
+    {
+
+        myrigidBody.velocity = new Vector2(h * character_speed, myrigidBody.velocity.y);
+    }
+}
