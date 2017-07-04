3de9ac5930df70cd5a8b50e7cccc0ded8c581c6b
afdhal-PC\afdhal <awakenblueheart@gmail.com>
2017-07-04 15:15:37 +0800

    Video 4 , 5 , 6 - Flip character right/left , Running animation (insert parameter "speed") Attack animation (attack,done attack animation, next attack)(insert trigger "attack")

--- first_game/FirstGame/Assets/Scripts/Player.cs
+++ first_game/FirstGame/Assets/Scripts/Player.cs
@@ -4,16 +4,25 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     private Rigidbody2D myrigidBody;
+    private Animator myanimator;
     [SerializeField]
     private float character_speed = 3;
     private bool facingRight;
+    private bool attack;
 
 	// Use this for initialization
 	void Start () {
+        facingRight = true;
         myrigidBody = GetComponent<Rigidbody2D>();
+        myanimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+
+    }
 	void FixedUpdate () {
         float Horizontal = Input.GetAxis("Horizontal");
 
@@ -25,18 +34,69 @@ public class Player : MonoBehaviour {
         //    Horizontal += 3;
         //}
         Debug.Log(Horizontal);
-
+        
         HandleMovement(Horizontal);
+        flip(Horizontal);// right/left
+        HandleAttack();
+        ResetValue();
 	}
 
     private void HandleMovement(float h)
     {
+        if (!this.myanimator.GetCurrentAnimatorStateInfo(0).IsTag("attack")) //animator->layer->firstlayer / tag on animation
+        {
+            myrigidBody.velocity = new Vector2(h * character_speed, myrigidBody.velocity.y);
 
-        myrigidBody.velocity = new Vector2(h * character_speed, myrigidBody.velocity.y);
+        }
+        //else if (this.myanimator.GetCurrentAnimatorStateInfo(0).IsTag("attack"))
+        //{
+        //    myrigidBody.velocity = new Vector2(0, 0);
+        //}
+
+        myanimator.SetFloat("speed", Mathf.Abs(h)); //run animation
     }
 
-    private void flip(float flip)
+    private void HandleAttack()
     {
-
+        if (attack && !this.myanimator.GetCurrentAnimatorStateInfo(0).IsTag("attack"))
+        {
+            myanimator.SetTrigger("attack");
+            myrigidBody.velocity = Vector2.zero;
+        }
     }
+
+    private void HandleInput()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            attack = true;
+        }        
+        
+    }
+   
+
+    private void flip(float horizontal)
+    {
+        if (horizontal < 0 && facingRight == true)
+        {
+            facingRight = false;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+        if (horizontal > 0 && facingRight == false)
+        {
+            facingRight = true;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+    private void ResetValue()
+    {
+        attack = false;
+    }
+
 }
